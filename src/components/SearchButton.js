bcc305bb424c2380ef148babdee636ec5536a9b6
Russ yev <ruslanyev@yahoo.com>
2019-06-29 18:47:28 -0700

    added a button

---
+++ src/components/SearchButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './SearchButton.css'
+
+export default function SearchButton(props) {
+  return (
+    <button className="searchButton">
+    Click to view results
+    </button>
+  );
+}
