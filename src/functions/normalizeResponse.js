19ec590305a6d9b117b0b66856514b39f3019893
Russ yev <ruslanyev@yahoo.com>
2019-06-30 15:26:57 -0700

    added error response normalization

---
+++ src/functions/normalizeResponse.js
@@ -0,0 +1,21 @@
+// Boilerplate code for handling errors from the API.  If the error response
+// contains JSON then we return a rejected promise containing the decoded
+// JSON.  If the error doesn't contain JSON then we return a rejected promise
+// containing the status text.  If there is no error then we continue with
+// the promise chain.
+export const normalizeResponseErrors = res => {
+  if (!res.ok) {
+    if (
+        res.headers.has('content-type') &&
+        res.headers.get('content-type').startsWith('application/json')
+      ) {
+        return res.json().then(err => Promise.reject({code: res.status, message: err.message}));
+      }
+      
+    return Promise.reject({
+      code: res.status,
+      message: res.statusText
+    });
+  }
+  return res;
+};
