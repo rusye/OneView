6f923dfd319ad898a8e16ae207d504c76c790289
Russ yev <ruslanyev@yahoo.com>
2019-06-29 18:46:52 -0700

    updated wording in header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchButton from "./components/SearchButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Hello
+          <img
+            src="https://www.oneviewcommerce.com/hs-fs/hubfs/OneView_5_13_transparent-01-1.png?width=180&name=OneView_5_13_transparent-01-1.png"
+            className="App-logo"
+            alt="logo"
+          />
+        </h1>
       </header>
+      <SearchButton />
     </div>
   );
 }
