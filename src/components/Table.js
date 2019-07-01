aa2faf8e1159536e535475aba4d781aade9bc694
Russ yev <ruslanyev@yahoo.com>
2019-06-30 18:28:42 -0700

    added table data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Table.css";
 
 export default function Table(props) {
   return (
-    <div>some stuff</div>
-  )
+    <table>
+      <tbody>
+        <tr>
+          {props.columns.map(column => {
+            return <th key={column}>{column}</th>;
+          })}
+        </tr>
+      </tbody>
+
+      {props.rows.map(row => {
+        return (
+          <tbody key={row.id}>
+            <tr>
+              <td key={row.name}>{row.name}</td>
+              <td key={row.email}>{row.email}</td>
+              <td key={row.address.city}>{row.address.city}</td>
+              <td key={row.company.name}>{row.company.name}</td>
+            </tr>
+          </tbody>
+        );
+      })}
+    </table>
+  );
 }
